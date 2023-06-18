f8ab9779c54ff8f55dbd3373a18fe35ddf488128
ann3i99 <92704196+ann3i99@users.noreply.github.com>
2023-06-18 14:34:39 +0800

    new duck with no animation :(

--- CGAssignment/src/cgassignment/duck.java
+++ CGAssignment/src/cgassignment/duck.java
@@ -3,128 +3,112 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package cgassignment;
+
 import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.GLProfile;
 import com.jogamp.opengl.awt.GLCanvas;
-import com.jogamp.opengl.glu.GLU;
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Frame;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-import javax.swing.Timer;
+
 /**
  *
  * @author Anneisha
  */
-public class duck extends JPanel implements ActionListener {
-
-    private int x;
-    private int y;
-    private Timer timer;
-
-    public duck() {
-        x = 0;
-        y = 150;
-        setPreferredSize(new Dimension(500, 300));
-        timer = new Timer(10, this);
-        timer.start();
-    }
-
-    private void drawBird(Graphics2D g2d) {
-    g2d.setStroke(new BasicStroke(2.0f));
-    g2d.setColor(Color.YELLOW);
-
+public class duck implements GLEventListener {
     
-    // Head
-    g2d.fillOval(x + 43, y - 5, 20, 20);
-    g2d.setColor(Color.BLACK);
-    g2d.drawOval(x + 43, y - 5, 20, 20);
-    
-    // Eyes
-    int eyeX1 = x + 58;
-    int eyeY = y + 5;
-    int eyeDiameter = 6;  // Increase the diameter for bigger eyes
-    g2d.fillOval(eyeX1 - eyeDiameter / 2, eyeY - eyeDiameter / 2, eyeDiameter, eyeDiameter);
-    
-     // Beak     
-    int beakX1 = x + 60;
-    int beakY1 = y + 5;    // Adjust the y-coordinate to move the beak up
-    int beakX2 = x + 75;
-    int beakY2 = y + 12;   // Adjust the y-coordinate to move the beak up
-    g2d.drawLine(beakX1, beakY1, beakX2, beakY2);
-    g2d.drawLine(beakX2, beakY2, beakX1, beakY2);
-
-
-    
-    // Body
-    g2d.setColor(Color.YELLOW);
-    g2d.fillOval(x, y, 50, 30);
-    g2d.setColor(Color.BLACK);
-    g2d.drawOval(x, y, 50, 30);
-    
-    
-
-    // Wings
-//    g2d.drawLine(x + 20, y + 10, x - 10, y + 10);
-//    g2d.drawLine(x + 20, y + 10, x + 10, y - 10);
-//    g2d.drawLine(x + 20, y + 10, x + 30, y - 10);
-//    g2d.drawLine(x + 20, y + 10, x + 10, y + 30);
-
-    int wingX1 = x + 5;
-    int wingY1 = y + 15;
-    int wingX2 = x + 20;
-    int wingY2 = y + 5;
-    int wingX3 = x + 35;
-    int wingY3 = y + 15;
-    
-    g2d.drawLine(wingX1, wingY1, wingX2, wingY2);
-    g2d.drawLine(wingX2, wingY2, wingX3, wingY3);
-
- 
-    // Tail
-//    g2d.drawLine(x, y + 15, x - 10, y + 5);
-//    g2d.drawLine(x, y + 15, x - 10, y + 25);
-    
-}
-
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
-        drawBird(g2d);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        x++;
-        if (x > getWidth() + 50) {
-            x = -50;
-        }
-        repaint();
-    }
-
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Flying Bird");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.getContentPane().add(new duck());
-            frame.pack();
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
-        });
+        GLProfile profile = GLProfile.get(GLProfile.GL2);
+        GLCanvas canvas = new GLCanvas(new GLCapabilities(profile));
+        canvas.addGLEventListener(new duck());
+
+        JFrame frame = new JFrame("Duck Renderer");
+        frame.getContentPane().add(canvas);
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void init(GLAutoDrawable drawable) {
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable drawable) {
+    }
+
+    @Override
+    public void display(GLAutoDrawable drawable) {
+        GL2 gl = drawable.getGL().getGL2();
+
+        gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+        
+        // Draw the body (oval)
+        gl.glColor3f(1.0f, 1.0f, 0.0f);
+        gl.glBegin(GL2.GL_POLYGON);
+        float radiusX = 0.6f;
+        float radiusY = 0.4f;
+        int numSegments = 100;
+        for (int i = 0; i < numSegments; i++) {
+            float theta = (float) (2 * Math.PI * i / numSegments);
+            float x = (float) (radiusX * Math.cos(theta));
+            float y = (float) (radiusY * Math.sin(theta));
+            gl.glVertex2f(x - 0.3f, y);
+        }
+        gl.glEnd();
+        
+        // First pass: Draw the beak
+        gl.glColor3f(0.0f, 0.0f, 0.0f);
+        gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL); // Set polygon mode to fill for the beak
+        gl.glBegin(GL2.GL_TRIANGLES);
+        gl.glVertex2f(0.6f, 0.2f);
+        gl.glVertex2f(0.6f, 0.0f);
+        gl.glVertex2f(0.8f, 0.1f);
+        gl.glEnd();
+
+        // Draw the head (circle)
+        gl.glColor3f(1.0f, 1.0f, 0.0f);
+        gl.glBegin(GL2.GL_POLYGON);
+        float radius = 0.3f;
+        int numSeg = 100;
+        for (int i = 0; i < numSeg; i++) {
+            float theta = (float) (2 * Math.PI * i / numSeg);
+            float x = (float) (radius * Math.cos(theta));
+            float y = (float) (radius * Math.sin(theta));
+            gl.glVertex2f(x + 0.4f, y + 0.2f);
+        }
+        gl.glEnd();
+        
+        // Draw the eyes (black dots)
+        gl.glPointSize(6.0f);
+        gl.glColor3f(0.0f, 0.0f, 0.0f);
+        gl.glBegin(GL2.GL_POINTS);
+        gl.glVertex2f(0.55f, 0.25f);
+        gl.glEnd();
+
+        // Draw the beak (v shape)
+        //gl.glColor3f(0.0f, 0.0f, 0.0f);
+//        gl.glBegin(GL2.GL_TRIANGLES);
+//        gl.glVertex2f(0.6f, 0.2f);
+//        gl.glVertex2f(0.6f, 0.0f);
+//        gl.glVertex2f(0.9f, 0.1f);
+//        gl.glEnd();
+
+        // Draw the wings (letter V)
+//        gl.glColor3f(0.0f, 0.0f, 0.0f);
+//        gl.glLineWidth(2.0f);
+//        gl.glBegin(GL2.GL_LINES);
+//        gl.glVertex2f(0.3f, 0.2f); // Left arm of the V
+//        //gl.glVertex2f(0.3f, 0.0f);  // Right arm of the V
+//        gl.glEnd();
+
+        gl.glFlush();
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
     }
 }
-
